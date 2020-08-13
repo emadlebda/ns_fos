44f7c90b4a79b9daafc550bdfb7ba0a7def15af4
Emad A. Abulebda <emadabulebda@hotmail.com>
2020-08-13 13:40:50 +0300

    dashboard almost done 2

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,8 +1,7 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('tag') }}'><i class='nav-icon la la-question'></i> Tags</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('restaurant') }}'><i class='nav-icon la la-question'></i> Restaurants</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('order') }}'><i class='nav-icon la la-question'></i> Orders</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('product') }}'><i class='nav-icon la la-question'></i> Products</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('user') }}'><i class="nav-icon las la-users"></i> Users</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('restaurant') }}'><i class="nav-icon las la-utensils"></i>Restaurants</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('order') }}'><i class='nav-icon las la-book-open'></i> Orders</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('product') }}'><i class='nav-icon la la-product-hunt'></i> Products</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('category') }}'><i class='nav-icon la la-question'></i> Categories</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('user') }}'><i class='nav-icon la la-question'></i> Users</a></li>
